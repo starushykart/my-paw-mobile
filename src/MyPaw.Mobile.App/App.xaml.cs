22aa02d17e94020ecd20987a4c246ed4abeabff9
Artem Starushyk <artem.starushyk@idealscorp.com>
2023-12-03 21:21:38 +0200

    initial

---
+++ src/MyPaw.Mobile.App/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace MyPaw.Mobile.App;
+
+public partial class App : Application
+{
+    public App()
+    {
+        InitializeComponent();
+
+        MainPage = new AppShell();
+    }
+}
---
+++ src/MyPaw.Mobile.App/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace MyPaw.Mobile.App;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+    protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}

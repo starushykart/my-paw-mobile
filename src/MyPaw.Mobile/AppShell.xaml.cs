10824f964fca9610f246df976c3dcbabd988faea
Artem Starushyk <artem.starushyk@idealscorp.com>
2024-05-26 16:12:50 +0300

    init

---
+++ src/MyPaw.Mobile/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace MyPaw.Mobile;
+
+public partial class AppShell : Shell
+{
+    public AppShell()
+    {
+        InitializeComponent();
+    }
+}
---
+++ src/MyPaw.Mobile/Platforms/Android/MainActivity.cs
@@ -0,0 +1,12 @@
+﻿using Android.App;
+using Android.Content.PM;
+using Android.OS;
+
+namespace MyPaw.Mobile;
+
+[Activity(Theme = "@style/Maui.SplashTheme", MainLauncher = true, LaunchMode = LaunchMode.SingleTop,
+    ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode |
+                           ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize | ConfigChanges.Density)]
+public class MainActivity : MauiAppCompatActivity
+{
+}
